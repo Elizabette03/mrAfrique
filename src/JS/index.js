e7586f902cb5c8d781b6aa49a56580911125e9df
Elizabette N <ndukaelizabeth26@yahoo.com>
2024-07-08 08:55:07 +0100

    Toggle darkmode switch active, index UI resized

---
+++ src/JS/index.js
@@ -0,0 +1,7 @@
+
+// Dark Mode toggle switch
+const colorToggle = document.getElementById('color-toggle');
+
+colorToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark")
+})
