b25bbd22f2309d28e30779a9f2135e04df6400b4
Elizabette N <ndukaelizabeth26@yahoo.com>
2024-07-08 10:34:18 +0100

    side navigation mouse event addded

--- src/JS/index.js
+++ src/JS/index.js
@@ -5,3 +5,22 @@ const colorToggle = document.getElementById('color-toggle');
 colorToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark")
 })
+
+
+// Side Navigation bar
+const sidebar = document.getElementById("sidebar");
+const navItems = document.getElementById("nav-items") 
+
+sidebar.addEventListener("mouseenter", () => {
+  console.log('sidebar')
+  sidebar.classList.add("bg-[#060c03]")
+  navItems.classList.remove("text-accent-2/30")
+  navItems.classList.add("text-accent-2")
+})
+
+sidebar.addEventListener("mouseleave", () => {
+  console.log('sidebar')
+  sidebar.classList.remove("bg-[#060c03]")
+  navItems.classList.remove("text-accent-2")
+  navItems.classList.add("text-accent-2/30")
+})
