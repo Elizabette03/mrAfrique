13007ec13e7fdff080857382db730ad257bf6090
Elizabette N <ndukaelizabeth26@yahoo.com>
2024-07-08 12:22:42 +0100

    Added blur on nav hover

--- src/JS/index.js
+++ src/JS/index.js
@@ -9,17 +9,18 @@ colorToggle.addEventListener("click", () => {
 
 // Side Navigation bar
 const sidebar = document.getElementById("sidebar");
+const toBlur = document.getElementById("to-blur");
 const navItems = document.getElementById("nav-items") 
 
 sidebar.addEventListener("mouseenter", () => {
-  console.log('sidebar')
+  toBlur.classList.add("blur-sm")
   sidebar.classList.add("bg-[#060c03]")
   navItems.classList.remove("text-accent-2/30")
   navItems.classList.add("text-accent-2")
 })
 
 sidebar.addEventListener("mouseleave", () => {
-  console.log('sidebar')
+  toBlur.classList.remove("blur-sm")
   sidebar.classList.remove("bg-[#060c03]")
   navItems.classList.remove("text-accent-2")
   navItems.classList.add("text-accent-2/30")
